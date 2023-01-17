92834752b41020a6770ada40ba9b6961ef6cf1de
Tom-the-Bomb <thello522@gmail.com>
2023-01-17 09:13:14 -0500

    added hello world test

--- tests/test_brainfuck.rs
+++ tests/test_brainfuck.rs
@@ -3,6 +3,17 @@ mod tests {
     use brainfuck::{Brainfuck, Result};
 
     #[test]
+    fn test_hello_world() -> Result<()> {
+        let code = r#">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<+
+        +.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-
+        ]<+."#;
+        Brainfuck::new(code)
+            .execute()?;
+
+        Ok(())
+    }
+
+    #[test]
     fn test_sierpinski() -> Result<()> {
         let code = r#"
         ++++++++[>+>++++<<-]>++>>+<[-[>>+<<-]+>>]>+[
